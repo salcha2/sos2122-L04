847ab297d499dfb34f017276e4c1276b7961f12d
Salim Chaieri Chafai <salcha2@alum.us.es>
2023-02-02 20:10:40 +0100

    first working version

---
+++ index.js
@@ -0,0 +1,19 @@
+const cool = require("cool-ascii-faces");
+const express = require("express");
+
+
+const app = express();
+const port = 8080;
+
+
+app.get("/", (req,res)=>{
+    console.log("Requested / route");
+    res.send("<html><body><h1>"+ cool() +"</h1></body></html>");
+});
+
+app.listen(port, () => {
+    console.log(`Server TRULY ready at port: ${port}`);
+}); 
+
+//console.log(`Server TRULY ready at port: ${port}`)
+
